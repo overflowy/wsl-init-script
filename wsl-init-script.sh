913c3a4d596a7e85d299e3a0d4364add0cf393f1
overflowy <overflowy@riseup.net>
2023-02-19 13:01:27 +0100

    Add script

---
+++ wsl-init-script.sh
@@ -0,0 +1,207 @@
+#!/bin/bash -i
+
+# Description: First run script for WSL2 Ubuntu 22.04.1 LTS
+
+NEW_HOSTNAME=lambda
+
+DEPS=(
+    build-essential
+    libbz2-dev      # Python
+    libffi-dev      # Python
+    liblzma-dev     # Python
+    libreadline-dev # Python
+    libsqlite3-dev  # Python
+    libssl-dev      # Python
+    python3-pip     # Python
+    zlib1g-dev      # Python
+)
+
+PACKAGES=(
+    bat
+    fd-find
+    fzf
+    micro
+    ranger
+    ripgrep
+    zoxide
+)
+
+FISHER_PLUGINS=(
+    jethrokuan/fzf
+)
+
+PIPX_PACKAGES=(
+    pdm
+    poetry
+    black
+    ruff
+)
+
+ASDF_PLUGINS=(
+    python
+    nodejs
+    nim
+    # golang
+    # rust
+)
+
+RED='\033[0;31m'
+RESET='\033[0m'
+
+print() {
+    echo -e "${RED}$1${RESET}"
+}
+
+cd "$(dirname "$0")"
+
+install_deps() {
+    print "Updating packages..."
+    sudo apt update
+    sudo apt upgrade -y
+
+    print "Installing dependencies..."
+    for dep in "${DEPS[@]}"; do
+        print "Installing $dep..."
+        sudo apt -qq install "$dep" -y
+    done
+}
+
+install_packages() {
+    print "Installing packages..."
+    for package in "${PACKAGES[@]}"; do
+        print "Installing $package..."
+        sudo apt -qq install "$package" -y
+    done
+}
+
+install_pipx_packages() {
+    print "Installing pipx packages..."
+    sudo apt install pipx -y
+    pipx ensurepath
+    for package in "${PIPX_PACKAGES[@]}"; do
+        pipx install "$package"
+    done
+
+}
+
+install_fish() {
+    print "Installing fish..."
+    sudo apt install fish -y
+    print "Changing shell to fish..."
+    chsh -s $(which fish)
+    mkdir -p $HOME/.config/fish
+    tee $HOME/.config/fish/config.fish >/dev/null <<EOF
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting
+set -gx EDITOR micro
+set -U FZF_LEGACY_KEYBINDINGS 0
+set PATH $PATH /home/deneb/.local/bin
+
+function fzf-help
+    echo "Ctrl-o        Find a file."
+    echo "Ctrl-r        Search through command history."
+    echo "Alt-c         cd into sub-directories (recursively searched)."
+    echo "Alt-Shift-c   Alt-Shift-c	cd into sub-directories, including hidden ones."
+    echo "Alt-o         Open a file/dir using default editor ($EDITOR)"
+    echo "Alt-Shift-o   Open a file/dir using xdg-open or open command"
+end
+
+function cheat
+    curl cheat.sh/$argv
+end
+EOF
+
+    # Fisher plugin manager
+    print "Installing Fisher..."
+    fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/HEAD/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+
+    # Install fisher plugins
+    print "Installing fisher plugins..."
+    for plugin in "${FISHER_PLUGINS[@]}"; do
+        fish -c "fisher install $plugin"
+    done
+}
+
+install_asdf() {
+    print "Installing asdf..."
+    git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.11.1
+
+    # Configure for bash
+    echo . "$HOME/.asdf/asdf.sh" >>$HOME/.bashrc
+    echo . "$HOME/.asdf/completions/asdf.bash" >>$HOME/.bashrc
+    source $HOME/.bashrc
+
+    # Configure for fish (requires fish)
+    echo "source $HOME/.asdf/asdf.fish" >>$HOME/.config/fish/config.fish
+    mkdir -p $HOME/.config/fish/completions
+    ln -s $HOME/.asdf/completions/asdf.fish $HOME/.config/fish/completions
+}
+
+install_asdf_plugins() {
+    print "Installing asdf plugins..."
+    for plugin in "${ASDF_PLUGINS[@]}"; do
+        asdf plugin add $plugin
+    done
+
+    for plugin in "${ASDF_PLUGINS[@]}"; do
+        asdf install $plugin latest
+        asdf global $plugin latest
+    done
+}
+
+setup_wsl() {
+    print "Setting up WSL..."
+    sudo rm -f /etc/wsl.conf
+    sudo tee /etc/wsl.conf >/dev/null <<EOF
+[network]
+hostname=$NEW_HOSTNAME
+generateHosts=false
+generateResolvConf=false
+EOF
+
+    sudo rm -f /etc/resolv.conf
+    sudo tee /etc/resolv.conf >/dev/null <<EOF
+nameserver 1.1.1.1
+nameserver 1.0.0.1
+EOF
+    sudo chattr -f +i /etc/resolv.conf
+
+    sudo sed -i "s/$HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
+}
+
+fix_annoyalances() {
+    print "Fixing annoyances..."
+    mkdir $HOME/Projects
+    touch $HOME/.hushlogin
+
+    # Bat
+    ln -s $(which batcat) ~/.local/bin/bat
+
+    # Fd
+    ln -s $(which fdfind) ~/.local/bin/fd
+
+    # Fzf-keybindings
+    echo "set -U FZF_LEGACY_KEYBINDINGS 0" >>$HOME/.config/fish/config.fish
+
+    # Zoxide
+    echo "zoxide init fish | source" >>$HOME/.config/fish/config.fish
+}
+
+main() {
+    install_deps
+    install_packages
+    install_pipx_packages
+    install_fish
+    sudo apt update
+    install_asdf
+    install_asdf_plugins
+    setup_wsl
+    fix_annoyalances
+    print "Done! 🙌"
+    print "Please restart your WSL instance."
+}
+
+main
